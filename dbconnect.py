0d55121da1bcdf97b4be59a5be9528cb62bcbc89
banks1502 <dev@DESKTOP-1TPFQH1.localdomain>
2019-11-13 13:33:37 +0530

    dbconnect.py

---
+++ dbconnect.py
@@ -0,0 +1,12 @@
+import pymysql.cursors
+
+# mysql --user=root -p
+def connection():
+    conn = pymysql.connect(host='104.37.86.29', # your host, usually localhost
+                     user="xdenachw", # your username
+                      passwd="1k)72C2ojaMR(Z", # your password
+                      port = 3306,
+                      db="xdenachw") # name of the data base
+    c = conn.cursor()
+
+    return c, conn
