ceeddab60a44b1c15c1eec5fceb49afd0c7cd70e
banks1502 <38201228+banks1502@users.noreply.github.com>
2019-11-19 11:43:32 +0530

    remove credentials
    Add required credential

--- dbconnect.py
+++ dbconnect.py
@@ -1,15 +1,15 @@
 import pymysql.cursors
 
-MAIL_SERVER='mail.cloudaccess.net'
-MAIL_USER = 'info@bitesofjoy.in'
-MAIL_PASS = '3idiots@123'
+MAIL_SERVER=''
+MAIL_USER = ''
+MAIL_PASS = ''
 # mysql --user=root -p
 def connection():
-    conn = pymysql.connect(host='104.37.86.29', # your host, usually localhost
-                     user="xdenachw", # your username
-                      passwd="1k)72C2ojaMR(Z", # your password
-                      port = 3306,
-                      db="xdenachw") # name of the data base
+    conn = pymysql.connect(host='', # your host, usually localhost
+                     user="", # your username
+                      passwd="", # your password
+                      port = ,
+                      db="") # name of the data base
     c = conn.cursor()
 
     return c, conn
